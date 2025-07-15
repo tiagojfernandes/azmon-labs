8a57a743e228910d5d9bbea5855285824cf08729
Tiago Fernandes <tifernan@microsoft.com>
2025-07-15 08:59:14 +0100

    provider fix

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,21 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
-}
-
-provider "random" {
-  # No configuration needed
 }
